14bee6e80a9717a00fe18d0787327cf30055fe7f
0xd34df00d <0xd34df00d@gmail.com>
2015-12-24 02:21:21 +0300

    Added Params::BuildFunctor().

--- src/params.h
+++ src/params.h
@@ -59,4 +59,46 @@ namespace Params
 		detail::BuildValuesImpl (vec, std::forward<Tail> (tail)...);
 		return vec;
 	}
+
+	namespace detail
+	{
+		template<typename F, typename S>
+		struct Map : F, S
+		{
+			using F::operator();
+			using S::operator();
+
+			Map (F f, S s)
+			: F { std::forward<F> (f) }
+			, S { std::forward<S> (s) }
+			{
+			}
+		};
+
+		template<typename F, typename S>
+		Map<F, S> Augment (F&& f, S&& s)
+		{
+			return { f, s };
+		}
+	}
+
+	template<typename ValueType>
+	auto BuildFunctor ()
+	{
+		struct
+		{
+			ValueType operator() () const
+			{
+				return {};
+			}
+		} dummy;
+		return dummy;
+	}
+
+	template<typename ValueType, typename NodeType, typename... Tail>
+	auto BuildFunctor (NodeType, ValueType val, Tail&&... tail)
+	{
+		return detail::Augment ([val] (NodeType) { return val; },
+				BuildFunctor<ValueType> (std::forward<Tail> (tail)...));
+	}
 }
