7baf3030f533271deb3ce163e1af2b9c132a478f
0xd34df00d <0xd34df00d@gmail.com>
2015-12-24 02:22:01 +0300

    Removed old array-based AddValue/BuildValues.

--- src/params.h
+++ src/params.h
@@ -4,62 +4,6 @@
 
 namespace Params
 {
-	template<char Family, int Index, typename Vec, typename T>
-	void AddValue (Vec& vec, T value)
-	{
-		vec [BuildIndex<Family, Index> ()] = value;
-	}
-
-	template<typename Tree>
-	struct MaxSize;
-
-	template<char Family, int Index>
-	struct MaxSize<Node<Variable<Family, Index>>>
-	{
-		static constexpr size_t Value = BuildIndex<Family, Index> ();
-	};
-
-	template<UnaryFunction UF, typename Child>
-	struct MaxSize<Node<UnaryFunctionWrapper<UF>, Child>>
-	{
-		static constexpr size_t Value = MaxSize<Child>::Value;
-	};
-
-	template<BinaryFunction BF, typename Left, typename Right>
-	struct MaxSize<Node<BinaryFunctionWrapper<BF>, Left, Right>>
-	{
-		static constexpr size_t Value = MaxSize<Left>::Value > MaxSize<Right>::Value ? MaxSize<Left>::Value : MaxSize<Right>::Value;
-	};
-
-	template<typename... Args>
-	struct MaxSize<Args...>
-	{
-		static constexpr size_t Value = 0;
-	};
-
-	namespace detail
-	{
-		template<typename Vec>
-		void BuildValuesImpl (Vec&)
-		{
-		}
-
-		template<char Family, int Index, typename T, typename Vec, typename... Tail>
-		void BuildValuesImpl (Vec& vec, Node<Variable<Family, Index>>, T value, Tail&&... tail)
-		{
-			AddValue<Family, Index> (vec, value);
-			BuildValuesImpl (vec, std::forward<Tail> (tail)...);
-		}
-	}
-
-	template<typename ArrT, typename FT, typename... Tail>
-	auto BuildValues (Tail&&... tail)
-	{
-		std::array<ArrT, MaxSize<FT>::Value + 1> vec {{ 0 }};
-		detail::BuildValuesImpl (vec, std::forward<Tail> (tail)...);
-		return vec;
-	}
-
 	namespace detail
 	{
 		template<typename F, typename S>
