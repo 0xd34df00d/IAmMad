2ff9eac284b75174280bee17cdb7e233b59b8279
0xd34df00d <0xd34df00d@gmail.com>
2015-12-25 00:39:30 +0300

    Added {x,y,w}_3.

--- src/parse.h
+++ src/parse.h
@@ -11,26 +11,32 @@ namespace Parse
 	using X0 = Node<Variable<'x', 0>>;
 	using X1 = Node<Variable<'x', 1>>;
 	using X2 = Node<Variable<'x', 2>>;
+	using X3 = Node<Variable<'x', 3>>;
 
 	using Y0 = Node<Variable<'y', 0>>;
 	using Y1 = Node<Variable<'y', 1>>;
 	using Y2 = Node<Variable<'y', 2>>;
+	using Y3 = Node<Variable<'y', 3>>;
 
 	using W0 = Node<Variable<'w', 0>>;
 	using W1 = Node<Variable<'w', 1>>;
 	using W2 = Node<Variable<'w', 2>>;
+	using W3 = Node<Variable<'w', 3>>;
 
 	X0 x0;
 	X1 x1;
 	X2 x2;
+	X3 x3;
 
 	Y0 y0;
 	Y1 y1;
 	Y2 y2;
+	Y3 y3;
 
 	W0 w0;
 	W1 w1;
 	W2 w2;
+	W3 w3;
 
 	Node<Number<1>> _1;
 
