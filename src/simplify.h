48355865b09a4e90bcce61b0ad613f794749e341
0xd34df00d <0xd34df00d@gmail.com>
2015-12-25 00:50:15 +0300

    Smarter number nodes handling in simplification.

--- src/simplify.h
+++ src/simplify.h
@@ -5,49 +5,61 @@
 
 namespace Simplify
 {
-	template<typename T>
+	template<typename T, typename Enable = void>
 	struct Simplify
 	{
 		using Result_t = T;
 	};
 
 	template<typename T>
+	using SimplifyPrime = Simplify<T>;
+
+	template<typename T>
 	using Simplify_t = typename Simplify<T>::Result_t;
 
-	template<>
-	struct Simplify<Node<Mul, Node<Number<1>>, Node<Number<1>>>>
-	{
-		using Result_t = Node<Number<1>>;
-	};
+	template<typename Other>
+	using NotANum_t = std::enable_if_t<!IsNumberNode<Other>::value>;
 
 	template<typename Other>
-	struct Simplify<Node<Mul, Other, Node<Number<1>>>>
+	struct Simplify<Node<Mul, Other, Node<Number<1>>>, NotANum_t<Other>>
 	{
 		using Result_t = Simplify_t<Other>;
 	};
 
 	template<typename Other>
-	struct Simplify<Node<Mul, Node<Number<1>>, Other>>
+	struct Simplify<Node<Mul, Node<Number<1>>, Other>, NotANum_t<Other>>
 	{
 		using Result_t = Simplify_t<Other>;
 	};
 
-	template<>
-	struct Simplify<Node<Mul, Node<Number<0>>, Node<Number<0>>>>
+	template<typename Other>
+	struct Simplify<Node<Mul, Other, Node<Number<0>>>, NotANum_t<Other>>
 	{
 		using Result_t = Node<Number<0>>;
 	};
 
 	template<typename Other>
-	struct Simplify<Node<Mul, Other, Node<Number<0>>>>
+	struct Simplify<Node<Mul, Node<Number<0>>, Other>, NotANum_t<Other>>
 	{
 		using Result_t = Node<Number<0>>;
 	};
 
-	template<typename Other>
-	struct Simplify<Node<Mul, Node<Number<0>>, Other>>
+	template<int N, int M>
+	struct Simplify<Node<Mul, Node<Number<N>>, Node<Number<M>>>>
 	{
-		using Result_t = Node<Number<0>>;
+		using Result_t = Node<Number<N * M>>;
+	};
+
+	template<typename Other>
+	struct Simplify<Node<Mul, Other, Node<Number<-1>>>, NotANum_t<Other>>
+	{
+		using Result_t = Node<Neg, Other>;
+	};
+
+	template<typename Other>
+	struct Simplify<Node<Mul, Node<Number<-1>>, Other>, NotANum_t<Other>>
+	{
+		using Result_t = Node<Neg, Other>;
 	};
 
 	template<>
@@ -87,8 +99,27 @@ namespace Simplify
 		using Result_t = T;
 	};
 
+	namespace detail
+	{
+		template<typename T, int... Numbers>
+		struct IsNumberValue : std::false_type {};
+
+		template<int N, int... Rest>
+		struct IsNumberValue<Node<Number<N>>, N, Rest...> : std::true_type {};
+
+		template<int K, int N, int... Rest>
+		struct IsNumberValue<Node<Number<K>>, N, Rest...> : IsNumberValue<Node<Number<K>>, Rest...> {};
+
+		template<int K>
+		struct IsNumberValue<Node<Number<K>>> : std::false_type {};
+	}
+
 	template<BinaryFunction BF, typename Left, typename Right>
-	struct Simplify<Node<BinaryFunctionWrapper<BF>, Left, Right>>
+	struct Simplify<Node<BinaryFunctionWrapper<BF>, Left, Right>,
+			std::enable_if_t<
+					(!IsNumberNode<Left>::value && !detail::IsNumberValue<Right, -1, 0, 1>::value) ||
+					(!IsNumberNode<Right>::value && !detail::IsNumberValue<Left, -1, 0, 1>::value)
+				>>
 	{
 		using LeftPrime_t = Simplify_t<Left>;
 		using RightPrime_t = Simplify_t<Right>;
@@ -98,7 +129,7 @@ namespace Simplify
 		using Result_t = typename std::conditional_t<
 				IsFixedPoint,
 				LazyApply<Id>,
-				LazyApply<::Simplify::Simplify>
+				LazyApply<SimplifyPrime>
 			>::template Result_t<Node<BinaryFunctionWrapper<BF>, LeftPrime_t, RightPrime_t>>;
 	};
 }
