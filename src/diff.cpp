3b07670fc7ebc6f7e6efe80028bae15ef5f9fa45
0xd34df00d <0xd34df00d@gmail.com>
2016-02-12 05:16:39 +0300

    diff.cpp updates.

--- src/diff.cpp
+++ src/diff.cpp
@@ -11,51 +11,9 @@
 
 int main ()
 {
-	{
-		using df = Node<Cos,
-				Node<Variable<'x', 0>>
-			>::Derivative_t<'x', 0>;
-		std::cout << df::Print () << std::endl;
+	using namespace Parse;
 
-		std::array<double, MaxSize<df>::Value + 1> vec {{ 0 }};
-		AddValue<'x', 0> (vec, M_PI / 2);
-
-		std::cout << df::Eval (vec) << std::endl;
-	}
-
-	std::cout << std::endl;
-
-	{
-		using df = Node<Mul,
-				Node<Sin,
-					Node<Variable<'x', 0>>
-				>,
-				Node<Cos,
-					Node<Variable<'y', 0>>
-				>
-			>::Derivative_t<'x', 0>;
-
-		std::cout << df::Print () << std::endl;
-
-		std::array<double, MaxSize<df>::Value + 1> vec;
-		AddValue<'x', 0> (vec, 1);
-		AddValue<'y', 0> (vec, 2);
-
-		std::cout << df::Eval (vec) << std::endl;
-	}
-
-	std::cout << std::endl;
-
-	{
-		using df = Node<Mul,
-				  Node<Variable<'x', 0>>,
-				  Node<Variable<'y', 0>>
-			  >::Derivative_t<'x', 0>;
-
-		std::array<double, MaxSize<df>::Value + 1> vec {{ 0 }};
-		AddValue<'x', 0> (vec, 1);
-		AddValue<'y', 0> (vec, 2);
-		std::cout << df::Eval (vec) << std::endl;
-	}
+	using Formula_t = decltype (Sqrt (x0));
+	using D_t = Simplify::Simplify_t<VarDerivative_t<Formula_t, decltype (x0)>>;
 }
 
